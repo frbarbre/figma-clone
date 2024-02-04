5ef85efe9810bf125b89406ab8d6cdea1d7fc799
fbaConversio <fba@conversio.dk>
2024-02-04 19:47:55 +0100

    initital commit

---
+++ components/cursor/LiveCursors.tsx
@@ -0,0 +1,31 @@
+import Cursor from './Cursor';
+import { COLORS } from '@/constants';
+import { useOthers } from '@/liveblocks.config';
+
+// display all other live cursors
+const LiveCursors = () => {
+  /**
+   * useOthers returns the list of other users in the room.
+   *
+   * useOthers: https://liveblocks.io/docs/api-reference/liveblocks-react#useOthers
+   */
+  const others = useOthers();
+
+  return others.map(({ connectionId, presence }) => {
+    if (presence == null || !presence?.cursor) {
+      return null;
+    }
+
+    return (
+      <Cursor
+        key={connectionId}
+        color={COLORS[Number(connectionId) % COLORS.length]}
+        x={presence.cursor.x}
+        y={presence.cursor.y}
+        message={presence.message || ''}
+      />
+    );
+  });
+};
+
+export default LiveCursors;
